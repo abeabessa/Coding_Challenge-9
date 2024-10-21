eb0291cbb9165bf08fdee0e53e97d290a3b1a609
abeabessa <bbessamarinho@gmail.com>
2024-10-20 22:16:45 -0400

    Create Section class to manage books and availability

--- cc_9.js
+++ cc_9.js
@@ -21,3 +21,30 @@ class Book {
         this._isAvailable = status;
     }
 }
+
+// 2. Section Class
+class Section {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    getAvailableBooks() {
+        return this.books.filter(book => book.isAvailable).length;
+    }
+
+    listBooks() {
+        this.books.forEach(book => {
+            console.log(`${book.getDetails()} - Available: ${book.isAvailable}`);
+        });
+    }
+
+    calculateTotalBooksAvailable() {
+        return this.getAvailableBooks();
+    }
+}
+
